ef3248791e45964c5ff6ca402242422ed29fc276
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-05-15 16:54:18 -0300

    Criando gamificacao do aluno

---
+++ src/main/java/br/com/devmos/escola/dominio/selo/Selo.java
@@ -0,0 +1,39 @@
+package br.com.devmos.escola.dominio.selo;
+
+import br.com.devmos.escola.dominio.aluno.CPF;
+
+public class Selo {
+	
+	private CPF cpfAluno;
+	private String selo;
+	
+	
+	public CPF getCpfAluno() {
+		return cpfAluno;
+	}
+
+
+	public void setCpfAluno(CPF cpfAluno) {
+		this.cpfAluno = cpfAluno;
+	}
+
+
+	public String getSelo() {
+		return selo;
+	}
+
+
+	public void setSelo(String selo) {
+		this.selo = selo;
+	}
+
+
+	public Selo(CPF cpfAluno, String selo) {
+		super();
+		this.cpfAluno = cpfAluno;
+		this.selo = selo;
+	}
+	
+	
+
+}
