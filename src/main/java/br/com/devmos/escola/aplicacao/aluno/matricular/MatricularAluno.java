c79b91fe4463232ef05459519b6e7ff7b23a3dd0
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-05-18 19:18:49 -0300

    Adicionando log no metodo de matricular

--- src/main/java/br/com/devmos/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ src/main/java/br/com/devmos/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,12 +1,17 @@
 package br.com.devmos.escola.aplicacao.aluno.matricular;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.devmos.escola.dominio.aluno.Aluno;
 import br.com.devmos.escola.dominio.aluno.AlunoMatriculado;
+import br.com.devmos.escola.dominio.aluno.LogDeAlunoMatriculado;
 import br.com.devmos.escola.dominio.aluno.PublicadorEvento;
 import br.com.devmos.escola.dominio.aluno.RepositorioAlunos;
 
 public class MatricularAluno {
-	
+	private static final Logger logger = LoggerFactory.getLogger(MatricularAluno.class);
+
 	private final RepositorioAlunos repositorioAlunos;
 	private final PublicadorEvento publicador;
 	
@@ -18,6 +23,8 @@ public class MatricularAluno {
 	public void matricular(MatriculaAlunoDTO dados) {
 		Aluno aluno = dados.criarAluno(); 
 		repositorioAlunos.matricular(aluno);
+		
+		logger.info("Aluno " + aluno.getNome() + " matriculado");
 		
 		AlunoMatriculado evento = new AlunoMatriculado(aluno.getCpf());
 		publicador.publicar(evento);
