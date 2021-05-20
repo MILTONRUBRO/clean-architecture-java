3a823a6187541f8b0574bd1b9e7454c4773e383a
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-05-20 19:43:32 -0300

    Adicionando log para selo de aluno

--- src/main/java/br/com/devmos/escola/infra/selo/GeraSeloAlunoNovato.java
+++ src/main/java/br/com/devmos/escola/infra/selo/GeraSeloAlunoNovato.java
@@ -1,5 +1,9 @@
 package br.com.devmos.escola.infra.selo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.devmos.escola.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.devmos.escola.dominio.Evento;
 import br.com.devmos.escola.dominio.aluno.AlunoMatriculado;
 import br.com.devmos.escola.dominio.aluno.CPF;
@@ -8,6 +12,8 @@ import br.com.devmos.escola.dominio.selo.RepositorioSelos;
 import br.com.devmos.escola.dominio.selo.Selo;
 
 public class GeraSeloAlunoNovato extends Ouvinte{
+	
+	private static final Logger logger = LoggerFactory.getLogger(GeraSeloAlunoNovato.class);
 	
 	private final RepositorioSelos repositorioSelos;
 
@@ -21,6 +27,7 @@ public class GeraSeloAlunoNovato extends Ouvinte{
 		CPF cpfAluno = ((AlunoMatriculado) evento).getCpfAluno();
 		Selo novato = new Selo(cpfAluno, "Novato");
 		repositorioSelos.adicionar(novato);
+		logger.info("Selo do aluno " + cpfAluno + " criado com sucesso");
 		
 	}
 
